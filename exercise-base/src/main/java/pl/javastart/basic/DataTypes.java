0ee6b5b0bb9869e53cd6badbe06c8d4dc94bf52d
Maciej Piotrowski <nishi@wimiip.pl>
2017-02-07 18:45:34 +0100

    3. Data types + homework (printing of diferrent data types)

---
+++ exercise-base/src/main/java/pl/javastart/basic/DataTypes.java
@@ -0,0 +1,21 @@
+package pl.javastart.basic;
+
+/**
+ * Created by nishi on 2017-02-07.
+ */
+public class DataTypes {
+  public static void main(String args[]) {
+    System.out.print("Number int: ");
+    System.out.println(25);
+
+    System.out.print("Number double: ");
+    System.out.println(27.5);
+
+    System.out.print("Character A: ");
+    System.out.print('A');
+    System.out.print("\n");
+
+    System.out.print("Do I learn programming? ");
+    System.out.println(true);
+  }
+}
---
+++ exercise-base/src/main/java/pl/javastart/basic/DataTypesHomework.java
@@ -0,0 +1,13 @@
+package pl.javastart.basic;
+
+/**
+ * Created by nishi on 2017-02-07.
+ */
+public class DataTypesHomework {
+  public static void main(String args[]) {
+    System.out.println("Hello Maciek, I heard that you are " + 25 + " and learn programming, is it " + true + '?');
+    System.out.println(true);
+    System.out.println("Are " + 'J' + " and " + 'K' + " initials yours?");
+    System.out.println("Yes, true");
+  }
+}
