c18548dfd8b57e74803e56408b1a8a9918949a8b
Maciej Piotrowski <nishi@wimiip.pl>
2017-02-07 18:11:33 +0100

    Initial commit Created structure of project + First program (Hello wordl) + Homework (display first and last name)

---
+++ exercise-base/src/main/java/pl/javastart/basic/HelloWorld.java
@@ -0,0 +1,10 @@
+package pl.javastart.basic;
+
+/**
+ * Created by nishi on 2017-02-07.
+ */
+public class HelloWorld {
+  public static void main(String args[]) {
+    System.out.println("Hello world! Witaj świecie!");
+  }
+}
