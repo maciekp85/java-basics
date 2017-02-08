183ef4a315870fa4bd60b11c7b4c68893b3c7d5d
Maciej Piotrowski <nishi@wimiip.pl>
2017-02-08 15:30:33 +0100

    6. Classes and objects - application

---
+++ library/src/main/java/pl/javastart/library/Book.java
@@ -0,0 +1,13 @@
+package pl.javastart.library;
+
+/**
+ * Created by nishi on 2017-02-08.
+ */
+public class Book {
+  String title;
+  String author;
+  int releaseDate;
+  int pages;
+  String publisher;
+  String isbn;
+}
--- library/src/main/java/pl/javastart/library/Library.java
+++ library/src/main/java/pl/javastart/library/Library.java
@@ -8,20 +8,21 @@ public class Library {
   public static void main(String args[]) {
     final String appName = "Library v0.1";
 
-    String title = "W pustyni i w puszczy";
-    String author = "Henryk Sienkiewicz";
-    int releaseDate = 2010;
-    int pages = 296;
-    String publisher = "Greg";
-    String isbn = "9788373271890";
+    Book book1 = new Book();
+    book1.title = "W pustyni i w puszczy";
+    book1.author = "Henryk Sienkiewicz";
+    book1.releaseDate = 2010;
+    book1.pages = 296;
+    book1.publisher = "Greg";
+    book1.isbn = "9788373271890";
 
     System.out.print(appName);
     System.out.println("Books are available in library: ");
-    System.out.println(title);
-    System.out.println(author);
-    System.out.println(releaseDate);
-    System.out.println(pages);
-    System.out.println(publisher);
-    System.out.println(isbn);
+    System.out.println(book1.title);
+    System.out.println(book1.author);
+    System.out.println(book1.releaseDate);
+    System.out.println(book1.pages);
+    System.out.println(book1.publisher);
+    System.out.println(book1.isbn);
   }
 }
