229491c7f430a370214918f6061c2d121e5ec56c
Maciej Piotrowski <nishi@wimiip.pl>
2017-02-08 17:30:16 +0100

    7. Methods and constructors - application (added constructor and method that prints info about book)

--- library/src/main/java/pl/javastart/library/Book.java
+++ library/src/main/java/pl/javastart/library/Book.java
@@ -4,10 +4,26 @@ package pl.javastart.library;
  * Created by nishi on 2017-02-08.
  */
 public class Book {
+
   String title;
   String author;
   int releaseDate;
   int pages;
   String publisher;
   String isbn;
+
+  Book(String bookTitle, String bookAuthor, int bookRelease, int bookPages, String bookPublisher, String bookIsbn) {
+    title = bookTitle;
+    author = bookAuthor;
+    releaseDate = bookRelease;
+    pages = bookPages;
+    publisher = bookPublisher;
+    isbn = bookIsbn;
+  }
+
+  void printInfo() {
+    String info = title + "; " + author + "; " + releaseDate + "; " + pages + "; " + publisher
+            + "; " + isbn;
+    System.out.println(info);
+  }
 }
